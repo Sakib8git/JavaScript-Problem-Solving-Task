f562d864c6554f0b8bce8144309ad10b4c5e03de
Sakib8git <sakib30278@gnail.com>
2026-01-07 21:59:50 +0600

    All problem solve

--- index2.js
+++ index2.js
@@ -1,7 +1,4 @@
-//! Problem 1: Reverse a String
-const srt = "hello";
-const reverseSrt = srt.split("").reverse().join("");
-console.log(reverseSrt);
+
 
 //! Problem 2: Count Vowels in a String
 const word = "programming";
@@ -27,11 +24,6 @@ if (input === reverseInput) {
   console.log(false);
 }
 
-//! Problem 4: Find the Maximum Number
-const numSet = [5, 1, 9, 30];
-const maxNum = Math.max(...numSet);
-console.log(maxNum);
-
 //! Problem 5: Remove Duplicates from an Array
 const numArray = [1, 2, 2, 3, 4, 4];
 
@@ -43,14 +35,6 @@ for (let num of numArray) {
 }
 console.log(newArray);
 
-// ! Problem 6: Sum of All Numbers in an Array
-const numArray2 = [1, 2, 3, 4];
-let sumOfNums = 0;
-for (let num of numArray2) {
-  sumOfNums = sumOfNums + num;
-}
-console.log(sumOfNums);
-
 // !Problem 7: Find Even Numbers in an Array
 const numArray3 = [1, 2, 3, 4, 5, 6];
 let evenNum = [];
@@ -61,25 +45,6 @@ for (let num of numArray3) {
   }
 }
 console.log(evenNum);
-
-//! Problem 8: Capitalize First Letter of Each Word
-const words = "hello world sakib";
-const splitWord = words.split(" ");
-// console.log(splitWord);
-const capWord = splitWord.map((word) => {
-  //   console.log(word);
-  return word.charAt(0).toUpperCase() + word.slice(1);
-});
-const newWword = capWord.join(" ");
-console.log(newWword);
-
-// ! Problem 9: Find the Factorial of a Number
-const num = 5;
-let facValue = 1;
-for (let i = num; i > 0; i--) {
-  facValue = facValue * i;
-}
-console.log(facValue);
 
 // ! Problem 10: PingPong Challenge
 const numArray4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 15];
